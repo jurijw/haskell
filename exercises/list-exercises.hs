812b5be1970593297a4a903cb0ea3070e1647daf
Jurij Waite <jurijlwaite@gmail.com>
2024-06-17 18:20:48 +0700

    Added an alternative solution to the first list exercise.

--- exercises/list-exercises.hs
+++ exercises/list-exercises.hs
@@ -1,5 +1,7 @@
 -- List exercises!
 
+import Data.List 
+
 -- Create a function myElem that returns True if an element is in a given list and returns False otherwise
 myElem :: (Eq a) => a -> [a] -> Bool
 
@@ -8,3 +10,11 @@ myElem target [] = False
 myElem target (x:xs)
   | x == target  = True
   | otherwise    = myElem target xs 
+
+-- Another approach using some of the povided convenience functions
+-- This approach creates a new list containing only elements that match
+-- the target and then checks if that list is empty or not.
+
+myElemAlt :: (Eq a) => a -> [a] -> Bool
+myElemAlt target inputList = not (null filteredList)
+  where filteredList = [x | x <- inputList, x == target]
